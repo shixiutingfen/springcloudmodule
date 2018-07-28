7b2550fda28d4a06d22cf1f3702bc16ef450cd5a
shixiuting <2628291375@qq.com>
2018-07-28 15:15:55 +0800

    Initial commit

---
+++ module1/src/main/java/com/jiuling/Module1Application.java
@@ -0,0 +1,20 @@
+package com.jiuling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Module1Application {
+	@GetMapping("/service")
+	public String service(){
+		return "service";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Module1Application.class, args);
+	}
+}
